46980f63b2913c0ba3fb4c57aac470a4360bf4bb
Hitko Development <development@hitko.si>
2017-09-03 20:32:52 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# gulp-js-text-imports
+# Gulp JS text imports
 Gulp plugin to inline `import name from './file.html';` as `const name = "content of file.html file";`, with support for content transformations
+
+# Install
+```
+npm install --save-dev gulp-js-text-imports
+```
+
+# Usage
+```js
+const gulp = require('gulp');
+const importsInliner = require('gulp-js-text-imports');
+const htmlMinify = require('html-minifier').minify;
+
+gulp.task('default', () => {
+    return gulp.src('./src/**/*.js')
+        .pipe(importsInliner({
+            handlers: {
+                'html': (content, path, callback) => callback(null, htmlMinify(content, {
+                    collapseWhitespace: true
+                }))
+            }
+        }))
+        .pipe(gulp.dest('./dest'))
+})
+```
+
+## Config
+Gulp JS text imports plugin supports the following config options:
+
+#### `acceptedExtensions: ['html', 'css']`
+Files with listed extensions will be inlined. Unles a coresponding handler is defined for an extension, the content of a file is escaped and inlined into the source file.
+
+#### `handlers: { extension: (content, path, callback) => callback(err, transformedContent) }`
+Optional handler functions to transform content prior to inlining, such as minifying HTML, and processing SASS.
+
+#### `parserOptions: { ecmaVersion: 7, sourceType: 'module' }`
+[Acorn](https://github.com/ternjs/acorn) parser options. This plugin is meant to work with ES6 module imports, therefore changing the `sourceType` option most likey won't work.
---
+++ index.js
@@ -0,0 +1,120 @@
+const acorn = require("acorn")
+const fs = require("fs")
+const path = require("path")
+const through = require('through2')
+const PluginError = require('gulp-util').PluginError
+
+const PLUGIN_NAME = 'gulp-js-text-imports'
+
+const noTransform = (content, path, callback) => callback(null, content)
+
+module.exports = (options) => {
+    options = options || {}
+    options.acceptedExtensions = options.acceptedExtensions || [
+        'html',
+        'css'
+    ]
+    options.handlers = options.handlers || {}
+    options.parserOptions = options.parserOptions || {
+        ecmaVersion: 7,
+        sourceType: 'module'
+    }
+    options.parserOptions.sourceType = options.parserOptions.sourceType || 'module'
+
+    let acceptedExtensions = Object.keys(options.handlers)
+    acceptedExtensions = acceptedExtensions.concat(
+        options.acceptedExtensions
+            .filter(ext => acceptedExtensions.indexOf(ext) < 0)
+    )
+    acceptedExtensions
+        .filter(ext => !options.handlers[ext])
+        .forEach(ext => options.handlers[ext] = noTransform)
+    options.acceptedExtensions = Object.keys(options.handlers)
+        .map(ext => ext.startsWith('.') ? ext : '.' + ext)
+
+    let transform = function (file, encoding, callback) {
+        if (file.isNull())
+            return callback(null, file)
+
+        if (file.isStream())
+            return this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'))
+
+        if (file.isBuffer()) {
+            let source = file.contents.toString()
+            let parsed = acorn.parse(source, options.parserOptions)
+            let processImports = parsed.body.filter(node => {
+                // Look for import declarations, importing files with specified extensions
+                if (
+                    node.type == 'ImportDeclaration'
+                    && node.source.type == 'Literal'
+                    && node.source.value
+                ) {
+                    let ext = path.extname(node.source.value).toLowerCase()
+                    if (options.acceptedExtensions.indexOf(ext) >= 0)
+                        return true
+                }
+                return false
+            }).filter(node => {
+                // Look for default imports, i.e. `import name from 'path'`
+                if (
+                    node.specifiers
+                    && node.specifiers.length
+                ) {
+                    let specifiers = node.specifiers.filter(specifier => specifier.type == 'ImportDefaultSpecifier')
+                    if (
+                        specifiers.length == 1
+                        && specifiers[0].local.type == 'Identifier'
+                    )
+                        return true
+                }
+                return false
+            }).map(node => {
+                return {
+                    matchStart: node.start,
+                    matchEnd: node.end,
+                    match: source.substr(node.start, node.end - node.start),
+                    targetFile: path.resolve(path.join(path.dirname(file.path), node.source.value)),
+                    targetVariable: node.specifiers.filter(specifier => specifier.type == 'ImportDefaultSpecifier')[0].local.name
+                }
+            }).map(imp => new Promise((resolve, reject) =>
+                fs.readFile(imp.targetFile, 'utf8', (err, content) => {
+                    if (err)
+                        return reject(err)
+
+                    let ext = path.extname(imp.targetFile)
+                        .substr(1)
+                        .toLowerCase()
+
+                    if (options.handlers[ext])
+                        options.handlers[ext](content, imp.targetFile, (err, newContent) => {
+                            if (err)
+                                return reject(err)
+
+                            imp.targetContent = newContent
+                            resolve(imp)
+                        })
+                    else {
+                        imp.targetContent = content
+                        resolve(imp)
+                    }
+                })
+            ))
+
+            Promise.all(processImports)
+                .then(imports => {
+                    imports
+                        .sort((a, b) => b.matchStart - a.matchStart)
+                        .forEach(imp => {
+                            source = source.substr(0, imp.matchStart)
+                                + 'const ' + imp.targetVariable + ' = ' + JSON.stringify(imp.targetContent || '') + ';'
+                                + source.substr(imp.matchEnd)
+                        })
+                    file.contents = new Buffer(source)
+                })
+                .then(() => callback(null, file))
+                .catch(err => callback(err, null))
+        }
+    }
+
+    return through.obj(transform)
+}
